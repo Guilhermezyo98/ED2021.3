d228d726de67692b5253ebfcb2b9f3337a1d661f
ArthurUFJF <arthurmpereira2010@gmail.com>
2021-10-28 17:57:38 -0300

    limpando codigo

--- leitura.h
+++ leitura.h
@@ -8,9 +8,9 @@ string pegaAteVirgula(string linha,int *marcador){
     string aux="";
     int saida=0;
     int aspas=0;
-    //cout<<linha.size()<<endl;
+
     while(saida==0 && *marcador!=linha.size()){
-        //cout<<*marcador<<endl;
+
         if(linha[*marcador]=='"'){
             if(aspas==0){
                 aspas=1;
@@ -21,32 +21,32 @@ string pegaAteVirgula(string linha,int *marcador){
         }
         if(linha[*marcador]==','){
             if(aspas==1){
-                //cout<<"testedsafs:"<<linha[*marcador];
+
                 aux=aux+linha[*marcador];
                 *marcador=*marcador+1;
             }
             else{
-                //cout<<"teste:"<<aux<<endl;
+
                 *marcador=*marcador+1;
                 return aux;
             }
         }
         else{
-            //cout<<"tpadr:"<<linha[*marcador];
+
             aux=aux+linha[*marcador];
             *marcador=*marcador+1;
         }
-        //cout<<*marcador;
+
     }
-    //cout<<"teste:"<<aux<<endl;
+
     return aux;
 }
 
 
 void leArquivo(string caminho,review *dados){
-    string linha,auxili;
+    string linha;
     fstream arquivo(caminho);
-    //string linha;
+
     int marcador=0;
     int i=0;
     if (!arquivo.is_open()){
@@ -56,16 +56,14 @@ void leArquivo(string caminho,review *dados){
     while (!arquivo.eof()){ //enquanto end of file for false continua
         marcador=0;
         getline(arquivo,linha);
-        //dados[i].id=pegaAteVirgula(linha,&marcador);
-        auxili=pegaAteVirgula(linha,&marcador);
-        //cout<<"erro arquivo nao aberto//"<<auxili<<endl;
-         dados[i].id=auxili;
-         dados[i].rev=pegaAteVirgula(linha,&marcador);
-         dados[i].votos=pegaAteVirgula(linha,&marcador);
-         dados[i].versao=pegaAteVirgula(linha,&marcador);
-         dados[i].data=pegaAteVirgula(linha,&marcador);
-         //imprimeReview(&dados[i]);
-         i++;
+
+        dados[i].id=pegaAteVirgula(linha,&marcador);
+        dados[i].rev=pegaAteVirgula(linha,&marcador);
+        dados[i].votos=pegaAteVirgula(linha,&marcador);
+        dados[i].versao=pegaAteVirgula(linha,&marcador);
+        dados[i].data=pegaAteVirgula(linha,&marcador);
+        //imprimeReview(&dados[i]);
+        i++;
       }
     arquivo.close();
 }
