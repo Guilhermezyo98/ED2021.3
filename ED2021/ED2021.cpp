8589273bb6dd6c5c8548f9b2c90daed955842625
DanielPortes <fagundes.daniel191@gmail.com>
2021-10-31 12:07:50 -0300

    varias mudancas

--- ED2021/ED2021.cpp
+++ ED2021/ED2021.cpp
@@ -14,9 +14,8 @@ struct Review;
 void eraseSubStr();
 void lerArquivoCSV();
 void imprimeReviewEspecifica();
-int _TAM_LINHAS = 3660725;
+int _TAM_LINHAS = 3'660'725;
 auto reviews = make_unique<Review[]>(_TAM_LINHAS);
-
 
 struct Review
 {
@@ -49,50 +48,94 @@ void lerArquivoCSV(const char* path)
 	}
 
 	string str;
-	arquivo.seekg(57, ios::cur); // pula primeira linha
+	arquivo.seekg(54, ios::cur); // pula primeira linha
 	int i = 0;
 
 	while (getline(arquivo, str))
 	{
-		if (str.find("gp:") == 0)
+		if (str.find("gp:") != string::npos)
 			// evita linhas ferradas,com quebra no meio do comentario, etc. Mas n ignora todas as ferradas
 		{
-			// pega ultima parte da string
-			auto date = str.substr(str.find_last_of(','));
-			eraseSubStr(str, date); // remove data coletada da linha
-			date.erase(0, 1); // remove virgula da data
-			reviews[i].posted_date = date;
-			cout << "posted_date: " << reviews[i].posted_date << "\n ";
+			auto pos = str.find_first_of(',');
+			if (pos != string::npos)
+			{
+				auto id = str.substr(0, pos);
+				eraseSubStr(str, id);
+				reviews[i].review_id = id;
+				str.erase(0, 1); // remove virgula
+				if (id.find("AOqpTOGzIxvnB1T6rW8cQKtWDx4v4Z3SpIVlg3s_6gIYpvhFB_OX") != string::npos)
+				{
+					__debugbreak();
+				}
+			}
 
-			// idem
-			auto version = str.substr(str.find_last_of(','));
-			eraseSubStr(str, version);
-			version.erase(0, 1);
-			reviews[i].app_version = version;
-			cout << "app_version: " << reviews[i].app_version << "\n ";
-
-			auto upvotes = str.substr(str.find_last_of(','));
-			eraseSubStr(str, upvotes);
-			upvotes.erase(0, 1);
-			reviews[i].upvotes = upvotes;
-			cout << "upvotes: " << reviews[i].upvotes << "\n ";
-
-			str.erase(0, 3); // remove gpa:
-			auto id = str.substr(0, str.find_first_of(','));
-			reviews[i].review_id = id;
-			cout << "review_id: " << reviews[i].review_id << "\n ";
-
-			// no fim a linha, str, so vai conter o review_text 
-			eraseSubStr(str, id);
-			str.erase(0, 1);
+			pos = str.find_last_of(',');
+			if (pos != string::npos)
+			{
+				auto date = str.substr(pos);
+				eraseSubStr(str, date);
+				reviews[i].posted_date = date;
+			}
+			pos = str.find_last_of(',');
+			if (pos != string::npos)
+			{
+				auto version = str.substr(pos);
+				eraseSubStr(str, version);
+				reviews[i].app_version = version;
+			}
+			pos = str.find_last_of(',');
+			if (pos != string::npos)
+			{
+				auto upvotes = str.substr(pos);
+				eraseSubStr(str, upvotes);
+				reviews[i].upvotes = upvotes;
+			}
 			reviews[i].review_text = str;
-			cout << "review_text: " << reviews[i].review_text << "\n ";
+
+			i++;
+		}
+		else
+		{
+			auto atual = ios::cur;
+			string novaLinha;
+			getline(arquivo, novaLinha);
+			if (novaLinha.find("gp:") != string::npos)
+			{
+				auto pos = str.find_last_of(',');
+				if (pos != string::npos)
+				{
+					auto date = str.substr(pos);
+					eraseSubStr(str, date);
+					reviews[--i].posted_date = date;
+				}
+				pos = str.find_last_of(',');
+				if (pos != string::npos)
+				{
+					auto version = str.substr(pos);
+					eraseSubStr(str, version);
+					reviews[--i].app_version = version;
+				}
+				pos = str.find_last_of(',');
+				if (pos != string::npos)
+				{
+					auto version = str.substr(pos);
+					eraseSubStr(str, version);
+					reviews[--i].app_version = version;
+				}
+			}
+			reviews[--i].review_text += str;
+			arquivo.seekg(0, atual);
 		}
 	}
 }
 
 void imprimeReviewEspecifica(int n)
 {
+	if (n < 0 || n > 3660725)
+	{
+		cout << endl << "fora do vetor" << endl;
+		return;
+	}
 	cout << "review_id: " << reviews[n].review_id << endl;
 	cout << "review_text: " << reviews[n].review_text << endl;
 	cout << "upvotes: " << reviews[n].upvotes << endl;
@@ -103,7 +146,7 @@ void imprimeReviewEspecifica(int n)
 int main()
 {
 	auto arquivoPath = "C:/workspace/DCC012/ED2/input/tiktok_app_reviews.csv";
-	
+
 	lerArquivoCSV(arquivoPath);
 
 	int entrada = 0;
---
+++ cmake-build-debug/ED2021/CMakeFiles/ED2021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ED2021.dir/ED2021.cpp.obj"
+  "ED2021.exe"
+  "ED2021.exe.manifest"
+  "ED2021.lib"
+  "ED2021.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ED2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
