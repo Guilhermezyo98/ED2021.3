b0f1521806f907fe4b418f8756fa372ab08a0f34
DanielPortes <fagundes.daniel191@gmail.com>
2021-11-06 13:53:46 -0300

    cmake files

---
+++ cmake-build-release/CMakeFiles/ED2021.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ED2021.dir/Timer.cpp.obj"
+  "CMakeFiles/ED2021.dir/main.cpp.obj"
+  "ED2021.exe"
+  "ED2021.exe.manifest"
+  "ED2021.lib"
+  "ED2021.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ED2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
