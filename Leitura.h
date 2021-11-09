5634f4db149e83f91bbde028ce74dd7e5ec5d76a
DanielPortes <fagundes.daniel191@gmail.com>
2021-11-09 15:54:18 -0300

    refatoracao, novos files

---
+++ Leitura.h
@@ -0,0 +1,24 @@
+#ifndef LEITURA_H
+#define LEITURA_H
+
+#include "cabecalhos_basicos.h"
+using namespace std;
+
+struct Review
+{
+	string review_id;
+	string review_text;
+	string upvotes;
+	string app_version;
+	string posted_date;
+};
+struct Review;
+void apagar_sub_str(std::string& main_str, const std::string& apagar);
+void lerArquivoCSV(const char* path, vector<Review>& reviews);
+void trataLinhasQuebradas(fstream& arquivo, vector<Review>& reviews, string& str, long& i);
+void imprimeReviewEspecifica(int n, vector<Review>& reviews);
+void escreveBin(Review* reviews);
+void leBin(Review* reviews);
+vector<Review>* importarReviewsAleatorios(int qtd);
+
+#endif // !LEITURA_H
---
+++ cabecalhos_basicos.h
@@ -0,0 +1,11 @@
+#ifndef CABECALHOS_BASICOS_H
+#define CABECALHOS_BASICOS_H
+
+#include <cassert>
+#include <fstream>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <vector>
+
+#endif // !CABECALHOS_BASICOS_H
