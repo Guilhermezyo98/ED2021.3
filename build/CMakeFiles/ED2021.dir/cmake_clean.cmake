fc7ec165437515271ff6cff0278d0d647a52bc09
DanielPortes <fagundes.daniel191@gmail.com>
2021-11-09 16:03:50 -0300

    cmake

---
+++ build/CMakeFiles/ED2021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ED2021.dir/Leitura.cpp.o"
+  "CMakeFiles/ED2021.dir/Timer.cpp.o"
+  "CMakeFiles/ED2021.dir/main.cpp.o"
+  "ED2021"
+  "ED2021.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ED2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
